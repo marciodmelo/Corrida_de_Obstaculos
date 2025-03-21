8525325f3b34c759cbab30eb149f2701c0aaa8f0
marciodemelo <marciodemelo@hotmail.com.br>
2025-03-21 20:08:52 -0300

    Menu funcional Background level Movimento Paralax

--- code/const.py
+++ code/const.py
@@ -3,6 +3,15 @@ COLOR_ORANGE = (255,128,0)
 COLOR_WHITE = (255,255,255)
 COLOR_YELlOW = (255, 234, 0)
 
+# E
+ENTITY_SPEED = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 2,
+    'Level1Bg4': 3,
+}
+
 # M
 MENU_OPTION = (
     'New Game',
--- code/game.py
+++ code/game.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 import pygame
 
-from code.const import WIN_WIDTH, WIN_HEIGHT
+from code.const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
+from code.level import Level
 from code.menu import Menu
 
 
@@ -14,5 +15,13 @@ class Game:
     def run(self):
         while True:
             menu = Menu(self.window)
-            menu.run()
-            pass
+            menu_return = menu.run()
+
+            if menu_return == MENU_OPTION[0]:
+                level = Level(self.window, 'Level1')
+                level_return = level.run()
+            elif menu_return == MENU_OPTION[1]:
+                pygame.quit()
+                quit()
+            else:
+                pass
